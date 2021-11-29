8a990e4207d8e0bd0877b2481c7645e59972192c
Alfredo Huerta <74021668+alfredohuerta@users.noreply.github.com>
2021-11-28 19:38:59 -0600

    Se completó la solución

--- Act 4.1 - Grafos, sus representaciones y sus recorridos/graph.h
+++ Act 4.1 - Grafos, sus representaciones y sus recorridos/graph.h
@@ -6,14 +6,16 @@
 *  las actividades planteadas en el curso TC1031
 *
 *
-*  Extiende este archivo para completar tu soución.
+*  Extiende este archivo para completar tu solución.
+* 
+* Alumno: Erick Alfredo García Huerta - A01708119
 */
 
 #ifndef Graph_H_
 #define Graph_H_
 
 #include <string>
-#include <stdio.h>
+#include <cstdio>
 #include <sstream>
 #include <fstream>
 #include <iostream>
@@ -33,42 +35,37 @@ private:
 		int nodes;
     vector<int> *adjList;
 		int *adjMatrix;
-		//vector<Type> *vect = new vector<Type>
 
 public:
-		void loadGraphMat(string, int, int);
-		Graph(int);
-		Graph();
-		void addEdgeAdjMatrix(int, int);
-		string printAdjMat();
-		string printAdjMat_clean();
+		Graph();//
+		Graph(int);//
+		void loadGraphMat(string, int, int);//
+		void loadGraphList(const string &, int, int);
+
+		void addEdgeAdjMatrix(int, int);//
+		void addEdgeAdjList(int, int);//
+
+		string printAdjMat();//
+		string printAdjMat_clean();//
+		string printAdjList();//
+
+		string DFS(int, int);
+		string BFS(int, int);
+
+		void dfsHelper(int, int, stack<int> &, list<int> &, vector<vector<int>> &, stringstream &);
+    	void bfsHelper(int, int, queue<int> &, list<int> &, vector<vector<int>> &, stringstream &);
+
+		static void printPath(vector<vector<int>> &, int, int, stringstream &);
+    	static void printVisited(list<int>, stringstream &);
+
 		bool contains(list<int>, int);
 		void sortAdjList();
 };
 
-
-void Graph::loadGraphMat(string name, int a, int b){
-	adjMatrix = new int [a*b];
-	nodes = a;
-	for (int i = 0; i < a*b; i++)
-		adjMatrix[i] = 0;
-		string line;
-		ifstream lee (name);
-		int u, v;
-		if (lee.is_open()){
-			while (getline(lee, line)){
-				u = stoi(line.substr(1,1));
-				v = stoi(line.substr(4,1));
-				addEdgeAdjMatrix(u,v);
-			}
-			lee.close(); // Closes the file
-		} else {
-			cout << "Unable to open file";
-		}
-}
-
 Graph::Graph() {
-	edgesList = edgesMat = 0;
+	edgesList = edgesMat = nodes = 0;
+	adjList= {};
+	adjMatrix= 0;
 }
 
 Graph::Graph(int n) {
@@ -80,13 +77,60 @@ Graph::Graph(int n) {
 	edgesList = edgesMat = 0;
 }
 
+void Graph::loadGraphMat(string name, int a, int b){
+	adjMatrix = new int [a*b];
+	nodes = a;
+	for (int i = 0; i < a*b; i++){
+		adjMatrix[i] = 0;
+	}	
+	string line;
+	ifstream lee (name);
+	int u, v;
+	if (lee.is_open()){
+		while (getline(lee, line)){
+			u = stoi(line.substr(1,1));
+			v = stoi(line.substr(4,1));
+			addEdgeAdjMatrix(u,v);
+		}
+		lee.close(); // Closes the file
+	} else {
+		cout << "Unable to open file";
+	}
+}
+
+void Graph::loadGraphList(const string &file_name, int a, int b){
+	nodes = a;
+    adjList = new vector<int>[nodes];
+
+    string line;
+    ifstream archivo(file_name);
+    int u, v;
+    if (archivo.is_open()) {
+        while (getline(archivo, line)) {
+            u = stoi(line.substr(1, 1));
+            v = stoi(line.substr(4, 1));
+            addEdgeAdjList(u, v);
+        }
+        archivo.close();
+	} else{
+		cout << "Unable to open file";
+	}
+}
+
 void Graph::addEdgeAdjMatrix(int u, int v){
 	adjMatrix[u*nodes+v] = 1;
 	adjMatrix[v*nodes+u] = 1;
 	edgesMat++;
 }
 
+void Graph::addEdgeAdjList(int u, int v){
+	adjList[u].push_back(v);
+	adjList[v].push_back(u);
+	edgesList++;
+}
+
 string Graph::printAdjList(){
+	  sortAdjList();
 	  stringstream aux;
 		for (int i = 0; i < nodes; i++){
 	        aux << "vertex "
@@ -96,7 +140,7 @@ string Graph::printAdjList(){
 					}
 	        aux << " ";
     }
-		return aux.str();
+	return aux.str();
 
 }
 
@@ -129,6 +173,122 @@ string Graph::printAdjMat_clean(){
 	   aux << "\n";
   }
 	return aux.str();
+}
+
+
+string Graph::DFS(int start, int goal){
+	stringstream aux;
+
+	stack <int> st;
+	list <int> visited;
+	vector<vector<int>> paths(nodes, vector<int>(1, -1));
+	st.push(start);
+
+	dfsHelper(start, goal, st, visited, paths, aux);
+	printPath(paths, start, goal, aux);
+
+	return aux.str();
+}
+
+string Graph::BFS(int start, int goal){
+	stringstream aux;
+
+    queue<int> q;
+    list<int> visited;
+    vector<vector<int>> paths(nodes, vector<int>(0));
+    q.push(start);
+
+    bfsHelper(start, goal, q, visited, paths, aux);
+    printPath(paths, start, goal, aux);
+
+    return aux.str();
+}
+
+
+void Graph::dfsHelper(int current, int goal, stack<int> &st, list<int> &visited, vector<vector<int>> &paths, stringstream &aux){
+	if(current == goal){
+		printVisited(visited, aux);
+	} else if(st.empty()){
+		cout << "No se encontró." << endl;
+	} else{
+		current= st.top();
+		st.pop();
+		visited.push_back(current);
+		for(int i= 0; i < adjList[current].size(); i++){
+			if(!contains(visited, adjList[current][i])){
+				st.push(adjList[current][i]);
+				paths[adjList[current][i]][0] = current;
+			}
+		}
+		dfsHelper(current, goal, st, visited, paths, aux);
+	}
+}
+
+void Graph::bfsHelper(int current, int goal, queue<int> &q, list<int> &visited, vector<vector<int>> &paths, stringstream &aux){
+	if(current == goal){
+		printVisited(visited, aux);
+	} else if(q.empty()){
+		aux << " ";
+	} else{
+		current = q.front();
+		q.pop();
+		visited.push_back(current);
+		for(int i= 0; i < adjList[current].size(); i++){
+			if(!contains(visited, adjList[current][i])){
+				q.push(adjList[current][i]);
+				paths[adjList[current][i]].push_back(current);
+			}
+		}
+		bfsHelper(current, goal, q, visited, paths, aux);
+	}
+}
+
+void Graph::printPath(vector<vector<int>> &paths, int start, int goal, stringstream &aux){
+	aux << "path: ";
+	int node= paths[goal][0];
+
+	stack<int> alReves;
+	alReves.push(goal);
+
+	while(node != start){
+		alReves.push(node);
+		node= paths[node][0];
+	}
+
+	alReves.push(start);
+	while(!alReves.empty()){
+		aux << alReves.top();
+		alReves.pop();
+		if(!alReves.empty()){
+			aux << " ";
+		}
+	}
+}
+
+void Graph::printVisited(list<int> visited, stringstream &aux){
+	aux << "visited: ";
+	while(!visited.empty()){
+		aux << visited.front() << " ";
+		visited.pop_front();
+	}
+}
+
+bool Graph::contains(list<int> lista, int node){
+	list<int>::iterator it;
+
+	it= find(lista.begin(), lista.end(), node);
+
+	if(it != lista.end()){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+void Graph::sortAdjList(){
+	for (int i = 0; i < nodes; i++){
+		sort(adjList[i].begin(),adjList[i].end());
+	}
 }
 
 #endif /* Graph_H_ */
